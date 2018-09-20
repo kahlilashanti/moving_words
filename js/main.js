408acc45408ad4b84c20017289dcd89fecbda858
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2018-09-20 15:41:05 -0700

    initial build

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var animationEnd = (function(el) {
+  var animations = {
+    animation: 'animationend',
+    OAnimation: 'oAnimationEnd',
+    MozAnimation: 'mozAnimationEnd',
+    WebkitAnimation: 'webkitAnimationEnd',
+  };
+
+  for (var t in animations) {
+    if (el.style[t] !== undefined) {
+      return animations[t];
+    }
+  }
+})(document.createElement('p'));
+
+// document.getElementById('list').on(animationEnd, bounceOutLeft);
